b4595e001323267282ca356d498935437417ef57
Tucker Beauchamp <tbeauchamp@2u.com>
2021-03-26 13:48:46 -0700

    add index html and static dir

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
 app.get("*", function (req, res) {
   res.send("I am the heroku test application");
 });
